dbae1c2e2fc07c50aa46c34cb41d1b8beb63df74
donkeypagoda <christopherallenstewart@gmail.com>
2018-02-13 16:49:00 -0800

    following along, building constants, actions, and reducers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Sidebar } from "./containers/Sidebar"
+import { MessagesList } from "./containers/MessagesList"
+import { AddMessage } from "./containers/AddMessage"
 
 class App extends Component {
   render() {
     return (
       <div id="container">
-        <aside id="sidebar">Users</aside>
-          <section id="main">
-            <section id="messages-list">Messages List</section>
-            <section id="new-message">New Message</section>
+        <Sidebar />
+          <section>
+            <MessagesList />
+            <AddMessage />
           </section>
       </div>
     );
